595697bae33fd659011cf3a206728538e9ae23dc
Hester Knight <hesterknight@Hesters-MacBook-Pro.local>
2019-11-04 19:07:37 +0000

    mvp finished

--- src/App.js
+++ src/App.js
@@ -38,12 +38,12 @@ const [tigerScore, setTigerScore] =useState(0);
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={() => setLionScore(lionScore +1)}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={() => setLionScore(lionScore +6)}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={() => setLionScore(lionScore + 3)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={() => setTigerScore(tigerScore +6)}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={() => setTigerScore(tigerScore +3)}>Away Field Goal</button>
         </div>
       </section>
     </div>
